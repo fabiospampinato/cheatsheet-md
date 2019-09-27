2e980590611f7af19fd823b3a03a95850601919c
Fabio Spampinato <spampinabio@gmail.com>
2019-09-28 00:11:49 +0200

    Optimized images

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,7 +7,7 @@ import * as React from 'react';
 
 const Hero = () => (
   <div className="hero">
-    <img src="/static/images/icon.png" title="Cheatsheet.md's logo" />
+    <img src="/static/images/icon.png" title="Cheatsheet.md's logo" width="192" height="192" />
     <h1 className="title">Cheatsheet.md</h1>
     <h2 className="description">Markdown cheatsheets collection.</h2>
   </div>
