7fc8147136b971db577a2fcd00156930428c9647
Fabio Spampinato <spampinabio@gmail.com>
2019-09-28 00:11:27 +0200

    CSS: minifying on production

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 
 /* IMPORT */
 
-const TsconfigPathsPlugin = require ( 'tsconfig-paths-webpack-plugin' ),
+const OptimizeCSSAssetsPlugin = require ( 'optimize-css-assets-webpack-plugin' ),
+      TsconfigPathsPlugin = require ( 'tsconfig-paths-webpack-plugin' ),
       withSass = require ( '@zeit/next-sass' );
 
 /* CONFIG */
@@ -10,6 +11,8 @@ const config = withSass ({
   webpack ( config ) {
     if ( !config.resolve.plugins ) config.resolve.plugins = [];
     config.resolve.plugins.push ( new TsconfigPathsPlugin () );
+    if ( !config.optimization.minimizer ) config.optimization.minimizer = [];
+    config.optimization.minimizer.push ( new OptimizeCSSAssetsPlugin () );
     return config;
   },
   experimental: {
